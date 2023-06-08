8981476fbc0854cd9f65373aa95424d1205c7ebe
robin <robinnagpal.tiet@gmail.com>
2023-06-08 16:22:46 -0400

    Update courses queries

--- src/graphql/queries/course/summarizedGitCourses.ts
+++ src/graphql/queries/course/summarizedGitCourses.ts
@@ -1,10 +1,10 @@
-import { QuerySummarizedGitCoursesArgs } from '@/graphql/generated/graphql';
+import { QueryCoursesArgs } from '@/graphql/generated/graphql';
 
 import { getAllGitGitCoursesForSpace } from '@/helpers/course/gitCourseReader';
 import { prisma } from '@/prisma';
 import { Space } from '@prisma/client';
 
-export default async function summarizedGitCourses(_: any, args: QuerySummarizedGitCoursesArgs) {
+export default async function summarizedGitCourses(_: any, args: QueryCoursesArgs) {
   const { spaceId } = args;
   const space: Space = await prisma.space.findFirstOrThrow({ where: { id: spaceId } });
   return await getAllGitGitCoursesForSpace(space);
