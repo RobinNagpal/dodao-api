e9876a73f10e8b2009e0cf88cb3bdb6bd76edaae
robin <robinnagpal.tiet@gmail.com>
2023-06-18 22:15:38 -0400

    Fix lint error

--- src/graphql/queries/space/getSpaceWithIntegrations.ts
+++ src/graphql/queries/space/getSpaceWithIntegrations.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/prisma';
 
-export async function getSpaceWithIntegrations(spaceId: string): Promise<{}> {
+export async function getSpaceWithIntegrations(spaceId: string) {
   const space = await prisma.space.findUniqueOrThrow({ where: { id: spaceId } });
 
   const spaceIntegrations = await prisma.spaceIntegration.findUnique({ where: { spaceId } });
