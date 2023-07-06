d134a339fea88107c833e33ccca15ad6eee861bd
robin <robinnagpal.tiet@gmail.com>
2023-07-06 18:11:14 -0400

    Some more cleanups

--- src/graphql/mutations/share/textOnImage.ts
+++ src/graphql/mutations/share/textOnImage.ts
@@ -4,33 +4,42 @@ import path from 'path';
 import { PDFImage } from 'pdf-image';
 import PDFDocument from 'pdfkit';
 
-export function writeTextToPdf(backgroundImagePath: string, contents: { title: string; content: string }[], outputPath: string) {
-  const doc: PDFKit.PDFDocument = new PDFDocument({
-    autoFirstPage: false,
+export function writeTextToPdf(backgroundImagePath: string, contents: { title: string; content: string }[], outputPath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    try {
+      const doc: PDFKit.PDFDocument = new PDFDocument({
+        autoFirstPage: false,
+      });
+
+      const stream = fs.createWriteStream(outputPath);
+      doc.pipe(stream);
+      const imgSize = sizeOf(backgroundImagePath);
+
+      doc.font('Helvetica');
+      doc.fontSize(100);
+      contents.map((content) => {
+        doc.addPage({ size: [imgSize.width!, imgSize.height!] });
+        doc.image(backgroundImagePath, 0, 0);
+
+        doc.text(content.title, 20, 220, {
+          width: imgSize.width! - 40,
+          align: 'center',
+        });
+
+        doc.text(content.content, 20, 370, {
+          width: imgSize.width! - 40,
+          align: 'center',
+        });
+      });
+      doc.end();
+
+      stream.on('finish', () => resolve());
+      stream.on('error', (err) => reject(err));
+    } catch (error) {
+      reject(error);
+    }
   });
-
-  doc.pipe(fs.createWriteStream(outputPath));
-  const imgSize = sizeOf(backgroundImagePath);
-
-  doc.font('Helvetica');
-  doc.fontSize(100);
-  contents.map((content) => {
-    doc.addPage({ size: [imgSize.width!, imgSize.height!] });
-    doc.image(backgroundImagePath, 0, 0);
-
-    doc.text(content.title, 20, 220, {
-      width: imgSize.width! - 40,
-      align: 'center',
-    });
-
-    doc.text(content.content, 20, 370, {
-      width: imgSize.width! - 40,
-      align: 'center',
-    });
-  });
-  doc.end();
 }
-
 // pdf2Image();
 async function extractImagesFromPdf() {
   const pdfPath = path.resolve('output.pdf');
