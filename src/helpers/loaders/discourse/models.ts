b7a750d9739a6837338c2b1cc575db57c01d42bc
robin <robinnagpal.tiet@gmail.com>
2023-08-14 18:27:53 -0400

    Some cleanups

--- src/helpers/loaders/discourse/models.ts
+++ src/helpers/loaders/discourse/models.ts
@@ -1,28 +1,6 @@
-import { Comment } from '@/helpers/loaders/discourse/getDiscoursePostWithComments';
-
-export interface DiscoursePost {
-  [x: string]: any;
-
-  source: string;
-  url: string;
-  fullContent: string;
-  author: string;
-  datePublished: string;
-  comments: Comment[];
-}
-
-export interface DiscourseThread {
-  url: string;
-  postContentFull: string;
-  author: string;
-  // dateElement: any;
-  date: string;
-  comments: Comment[];
-}
-
-export interface DiscourseIndexRunWithPosts {
-  id: number;
-  url: string;
-  runDate: Date;
-  posts: DiscoursePost[];
+export enum PostStatus {
+  NEEDS_INDEXING = 'NEEDS_INDEXING',
+  STARTED_INDEXING = 'STARTED_INDEXING',
+  INDEXING_FAILED = 'INDEXING_FAILED',
+  INDEXING_SUCCESS = 'INDEXING_SUCCESS',
 }
