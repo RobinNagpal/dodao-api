6db12eb3b4c2172a1e05d3fb45028b7d5233c730
Harisudarsan <sudarshanhari561@gmail.com>
2023-08-16 17:48:03 +0530

    message fetch in sucessful
    1) messages fetched whenever message is created

--- src/helpers/loaders/discord/index.ts
+++ src/helpers/loaders/discord/index.ts
@@ -1,25 +1,20 @@
-import { Client, IntentsBitField, TextChannel, Message } from 'discord.js';
-import dotenv from "dotenv"
-import { options } from 'pdfkit';
-
-dotenv.config()
-const client = new Client({ intents: IntentsBitField.Flags.Guilds })
-client.login(process.env.DISCORD_BOT)
+import { Message } from 'discord.js';
+import client from './client'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag}`);
 });
-// client.on("message", msg => {
-//   console.log(msg)
-// })
+client.on("messageCreate", (msg: Message) => {
+  console.log(msg.content)
+})
 //
 // const res = loadData(["1141046406153502733", "1141064439639048213"], client)
 // console.log(res)
-client.on('message', async (message: Message) => {
-
-  console.log(message.channel.awaitMessages())
-
-});
+// client.on('message', async (message: Message) => {
+//
+//   console.log(message.channel.awaitMessages())
+//
+// });
 //
 // const res = client.channels.fetch("1141046406153502733")
 // console.log(res)
