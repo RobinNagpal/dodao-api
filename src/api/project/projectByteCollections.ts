8cb8d1f78dbeb04861876ad37cb04ba6860ca18b
robin <robinnagpal.tiet@gmail.com>
2023-10-23 13:48:48 -0400

    Project Byte collection fixes

--- src/api/project/projectByteCollections.ts
+++ src/api/project/projectByteCollections.ts
@@ -1,10 +1,11 @@
 import apiResponseWrapper from '@/helpers/api/apiResponseWrapper';
-import { prisma } from '@/prisma';
+import { getProjectBytesCollectionWithBytes } from '@/helpers/project/projectByteCollectionsHelper';
 import { Request, Response } from 'express-serve-static-core';
 
 async function projectByteCollections(req: Request, res: Response) {
   const projectId = req.params.projectId;
-  return { status: 200, body: await prisma.projectByteCollection.findMany({ where: { projectId: projectId } }) };
+
+  return { status: 200, body: await getProjectBytesCollectionWithBytes(projectId) };
 }
 
 export default apiResponseWrapper(projectByteCollections);
--- src/helpers/project/projectByteCollectionsHelper.ts
+++ src/helpers/project/projectByteCollectionsHelper.ts
@@ -1,4 +1,7 @@
 import { ByteCollectionByte, ProjectByteCollection as ProjectByteCollectionGraphql } from '@/graphql/generated/graphql';
+import { logError } from '@/helpers/adapters/errorLogger';
+import { getByteCollectionRedisKey } from '@/helpers/byteCollection/byteCollectionsHelper';
+import { getRedisValue, setRedisValue } from '@/helpers/redis';
 import { prisma } from '@/prisma';
 import { ProjectByteCollection } from '@prisma/client';
 
@@ -24,4 +27,28 @@ export async function getProjectByteCollectionWithBytes(byteCollection: ProjectB
 
 export function getProjectByteCollectionRedisKey(spaceId: string) {
   return `project_byte_collections_${spaceId}`;
+}
+
+export async function getProjectBytesCollectionWithBytes(projectId: string) {
+  const projectByteCollections = await prisma.projectByteCollection.findMany({ where: { projectId: projectId } });
+
+  const redisKey = getByteCollectionRedisKey(projectId);
+
+  const byteCollection = await getRedisValue(redisKey);
+
+  if (byteCollection) {
+    return JSON.parse(byteCollection) as ProjectByteCollectionGraphql[];
+  }
+  const byteCollectionsWithBytes: ProjectByteCollectionGraphql[] = [];
+
+  for (const byteCollection of projectByteCollections) {
+    byteCollectionsWithBytes.push(await getProjectByteCollectionWithBytes(byteCollection));
+  }
+
+  try {
+    await setRedisValue(redisKey, JSON.stringify(byteCollectionsWithBytes));
+  } catch (e) {
+    logError('Error setting redis value', { error: e });
+  }
+  return byteCollectionsWithBytes;
 }
