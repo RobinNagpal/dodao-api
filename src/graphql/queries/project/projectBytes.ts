7bd5afb402e171e6cca19b7e16f77ecdeadbc5e4
robin <robinnagpal.tiet@gmail.com>
2023-11-06 13:29:35 -0500

    Fix fetching of bytes

--- src/graphql/queries/project/projectBytes.ts
+++ src/graphql/queries/project/projectBytes.ts
@@ -1,5 +1,6 @@
+import { QueryProjectBytesArgs } from '@/graphql/generated/graphql';
 import { prisma } from '@/prisma';
 
-export default function projectBytes() {
-  return prisma.projectByte.findMany();
+export default function projectBytes(_: any, { projectId }: QueryProjectBytesArgs) {
+  return prisma.projectByte.findMany({ where: { projectId: projectId } });
 }
