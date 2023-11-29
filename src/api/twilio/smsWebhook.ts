ce25a980943d8296ff20e67d3bf51deccab1f214
robin <robinnagpal.tiet@gmail.com>
2023-11-29 10:44:38 -0500

    Fix compilation error

--- src/api/twilio/smsWebhook.ts
+++ src/api/twilio/smsWebhook.ts
@@ -1,5 +1,3 @@
-import { prisma } from '@/prisma';
-
 import { Request, Response } from 'express-serve-static-core';
 import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 import { v4 } from 'uuid';
@@ -7,17 +5,6 @@ import { v4 } from 'uuid';
 const token = '';
 
 async function smsWebhook(req: Request, res: Response) {
-  prisma.twilioSms.create({
-    data: {
-      id: v4(),
-      sid: req.body.SmsSid,
-      updatedAt: new Date(),
-      from: req.body.From,
-      to: req.body.To,
-      body: req.body.Body,
-      direction: req.body.Direction,
-    },
-  });
   console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
 
   const twiml = new MessagingResponse();
