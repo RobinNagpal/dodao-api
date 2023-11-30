44b60c20b7b8e92eadbb291ceb3d36d0f4e979f6
robin <robinnagpal.tiet@gmail.com>
2023-11-30 15:48:40 -0500

    Fasten the code

--- src/api/chat/helpers/getNormalizedMatches.ts
+++ src/api/chat/helpers/getNormalizedMatches.ts
@@ -1,9 +1,39 @@
 import { MatchedDocument } from '@/helpers/chat/matches';
+import { logError } from '@/helpers/errorLogger';
 import { getNormalizedEntries } from '@/helpers/loaders/getContentFromLoaderEntity';
 import { PageMetadata } from '@/types/chat/projectsContents';
 import uniqBy from 'lodash/uniqBy';
 
-export async function getNormalizedMatches(matches: MatchedDocument[], enacted: boolean, discussed: boolean) {
+/**
+ * If you don't understand this function, refer to the simpler serial version below.
+ * @param matches
+ * @param enacted
+ * @param discussed
+ */
+export async function getNormalizedMatches(matches: MatchedDocument[], enacted: boolean, discussed: boolean): Promise<PageMetadata[]> {
+  const uniqueMatches: MatchedDocument[] = uniqBy(matches, 'metadata.fullContentId');
+
+  // Map each match to a promise with explicit type annotation
+  const promises: Promise<PageMetadata | null | undefined>[] = uniqueMatches.map((match) =>
+    getNormalizedEntries(match.metadata, enacted, discussed).catch((e: Error): null => {
+      logError(e?.toString() || 'Error in getNormalizedMatches', {}, e as any, null, null);
+      return null;
+    }),
+  );
+
+  // Use Promise.allSettled to handle all promises in parallel
+  const results: PromiseSettledResult<PageMetadata | null | undefined>[] = await Promise.allSettled(promises);
+
+  // Filter out rejected promises and extract values from fulfilled ones
+  const successfulResults: PageMetadata[] = results
+    .filter((result): result is PromiseFulfilledResult<PageMetadata> => result.status === 'fulfilled' && result.value !== null)
+    .map((result) => result.value);
+
+  const uniqueNormalizedMatches: PageMetadata[] = uniqBy(successfulResults, 'fullContentId');
+  return uniqueNormalizedMatches;
+}
+
+export async function getNormalizedMatchesSerially(matches: MatchedDocument[], enacted: boolean, discussed: boolean) {
   const uniqueMatches = uniqBy(matches, 'metadata.fullContentId');
   const normalizedMatches: PageMetadata[] = [];
 
