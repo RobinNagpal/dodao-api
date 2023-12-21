cbd65dad5ff2b1fa091b00fd8eb80ac2c56c0e48
robin <robinnagpal.tiet@gmail.com>
2023-12-21 15:33:28 -0500

    Don't use redis

--- src/graphql/queries/project/projectByteCollections.ts
+++ src/graphql/queries/project/projectByteCollections.ts
@@ -1,6 +1,5 @@
 import { QueryProjectByteCollectionsArgs } from '@/graphql/generated/graphql';
 import { getProjectBytesCollectionWithBytes } from '@/helpers/project/projectByteCollectionsHelper';
-import { prisma } from '@/prisma';
 
 export default function projectByteCollections(_: any, { projectId }: QueryProjectByteCollectionsArgs) {
   return getProjectBytesCollectionWithBytes(projectId);
