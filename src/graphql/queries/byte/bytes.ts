3eb537e14bdd8572e28b3d6cf8da1d60e811ff4b
robin <robinnagpal.tiet@gmail.com>
2023-12-21 15:40:46 -0500

    Don't use db or redis for bytes

--- src/graphql/queries/byte/bytes.ts
+++ src/graphql/queries/byte/bytes.ts
@@ -1,23 +1,8 @@
-import { PublishStatus } from '@/deprecatedSchemas/models/enums';
-import { Byte, QueryBytesArgs } from '@/graphql/generated/graphql';
-import { AcademyObjectTypes } from '@/helpers/academy/academyObjectTypes';
-import { getAllAcademyObjectsForSpace } from '@/helpers/academy/readers/academyObjectReader';
+import { QueryBytesArgs } from '@/graphql/generated/graphql';
 import { prisma } from '@/prisma';
 
 export async function getBytes(spaceId: string) {
-  const bytesInRedis = (await getAllAcademyObjectsForSpace(spaceId, AcademyObjectTypes.bytes)) as Byte[];
-  const dbBytes = await prisma.byte.findMany({ where: { spaceId: spaceId } });
-
-  // replace bytes in redis with draft bytes
-  const combinedBytes = bytesInRedis.map((byte) => {
-    const dbByte = dbBytes.find((draftByte) => draftByte.id === byte.id);
-    return dbByte ? dbByte : byte;
-  });
-
-  // include bytes in DB that are not in Redis
-  const onlyInDBBytes = dbBytes.filter((dbByte) => !bytesInRedis.find((redisByte) => redisByte.id === dbByte.id));
-
-  return [...combinedBytes, ...onlyInDBBytes];
+  return prisma.byte.findMany({ where: { spaceId: spaceId } });
 }
 
 export default async function bytes(_: any, args: QueryBytesArgs) {
