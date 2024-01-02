054d2b421852f5d7e794986e41a2917cd27eaee8
robin <robinnagpal.tiet@gmail.com>
2024-01-02 10:42:22 -0500

    Use JSONB as the column type

--- prisma/migrations/20240102131153_add_new_columns/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "projects" ADD COLUMN     "admin_usernames_v1" JSON[] DEFAULT ARRAY[]::json[];
-
--- AlterTable
-ALTER TABLE "spaces" ADD COLUMN     "admin_usernames_v1" JSON[] DEFAULT ARRAY[]::json[];
