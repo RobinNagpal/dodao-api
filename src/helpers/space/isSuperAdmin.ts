a89002f800f9b0947c723d7bd9bbdbb9ae2ae15c
robin <robinnagpal.tiet@gmail.com>
2024-01-03 06:56:03 -0500

    Cleanup

--- src/helpers/space/isSuperAdmin.ts
+++ src/helpers/space/isSuperAdmin.ts
@@ -4,7 +4,7 @@ import { IncomingMessage } from 'http';
 import { JwtPayload } from 'jsonwebtoken';
 
 const superAdminsFromEnv = process.env.DODAO_SUPERADMINS ? process.env.DODAO_SUPERADMINS.split(',') : [];
-const superAdminsArray = [...superAdminsFromEnv, '0x470579d16401a36BF63b1428eaA7189FBdE5Fee9', 'robinnagpal.tiet@gmail.com'].map((u) => u.toLowerCase());
+const superAdminsArray = [...superAdminsFromEnv, '0x470579d16401a36BF63b1428eaA7189FBdE5Fee9', 'robinnagpal.tiet@gmail.com'].map((u) => u.toLowerCase().trim());
 export function isDoDAOSuperAdmin(context: IncomingMessage): (JwtPayload & DoDaoJwtTokenPayload) | null {
   const decoded = getDecodedJwtFromContext(context);
 
