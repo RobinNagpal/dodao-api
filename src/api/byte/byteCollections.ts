228d73eeb145da0cac49c00f3453c4631c65ff6f
robin <robinnagpal.tiet@gmail.com>
2024-01-15 08:14:54 -0500

    Add new API route for byte collections

---
+++ src/api/byte/byteCollections.ts
@@ -0,0 +1,10 @@
+import { getByteCollectionsForSpace } from '@/graphql/queries/byteCollection/byteCollections';
+import apiResponseWrapper from '@/helpers/api/apiResponseWrapper';
+import { Request, Response } from 'express-serve-static-core';
+
+async function byteCollections(req: Request, res: Response) {
+  const spaceId = req.params.spaceId;
+  return { status: 200, body: await getByteCollectionsForSpace(spaceId) };
+}
+
+export default apiResponseWrapper(byteCollections);
