98ea7064cd6b6aab044186fbdcadb6cbebeb6274
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-22 23:45:22 +0500

    formatting changes

--- src/graphql/mutations/project/updateSEOOfProjectShortVideo.ts
+++ src/graphql/mutations/project/updateSEOOfProjectShortVideo.ts
@@ -5,18 +5,18 @@ import { prisma } from '@/prisma';
 import { IncomingMessage } from 'http';
 
 export default async function updateSeoOfProjectShortVideo(parent: any, args: MutationUpdateSeoOfProjectShortVideoArgs, context: IncomingMessage) {
-    const spaceById = await prisma.space.findUniqueOrThrow({ where: { id: TOP_CRYPTO_PROJECTS_SPACE_ID } });
+  const spaceById = await prisma.space.findUniqueOrThrow({ where: { id: TOP_CRYPTO_PROJECTS_SPACE_ID } });
 
-    const decodedJwt = checkEditSpacePermission(spaceById, context);
+  const decodedJwt = checkEditSpacePermission(spaceById, context);
 
-    const upsertedProjectShortVideo = await prisma.projectShortVideo.update({
-        data: {
-            seoMeta: args.seoMeta ?? {},
-        },
-        where: {
-            id: args.projectId,
-        },
-    });
+  const upsertedProjectShortVideo = await prisma.projectShortVideo.update({
+    data: {
+      seoMeta: args.seoMeta ?? {},
+    },
+    where: {
+      id: args.projectId,
+    },
+  });
 
-    return upsertedProjectShortVideo;
+  return upsertedProjectShortVideo;
 }
