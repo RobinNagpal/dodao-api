06c06e50de2b4510e9c3569d0d43a2535088e268
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-14 19:34:58 +0500

    updated file

--- src/graphql/queries/shortVideo/shortVideo.ts
+++ src/graphql/queries/shortVideo/shortVideo.ts
@@ -1,11 +1,11 @@
 import { QueryShortVideoArgs } from '@/graphql/generated/graphql';
 import { prisma } from '@/prisma';
 
-export default function shortVideo(_: any, { spaceId, shortVideoId }: QueryShortVideoArgs) {
+export default function shortVideo(_: any, { spaceId, id }: QueryShortVideoArgs) {
   return prisma.shortVideo.findFirstOrThrow({
     where: {
       spaceId: spaceId,
-      id: shortVideoId,
+      id: id,
     },
   });
 }
