44912f6be71648aa1f75adfa4b6b3721e34a8d61
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-16 19:16:56 +0500

    added type

--- src/graphql/mutations/space/updateSpace.ts
+++ src/graphql/mutations/space/updateSpace.ts
@@ -39,6 +39,7 @@ export default async function updateSpace(_: unknown, args: MutationUpdateSpaceA
     socialSettings: space.socialSettings || {},
     byteSettings: space.byteSettings || {},
     themeColors: space.themeColors || null,
+    type: space.type,
   };
   try {
     console.log('Updating space', spaceInput);
