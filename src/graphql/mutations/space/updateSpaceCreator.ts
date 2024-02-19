7ff18b6a26b2e78253012405a8a641ca15a69880
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-19 20:50:00 +0500

    new mutation api

---
+++ src/graphql/mutations/space/updateSpaceCreator.ts
@@ -0,0 +1,18 @@
+import { MutationUpdateSpaceCreatorArgs } from '@/graphql/generated/graphql'
+import { getSpaceById } from '@/graphql/operations/space';
+import { checkEditSpacePermission } from '@/helpers/space/checkEditSpacePermission';
+import { prisma } from '@/prisma';
+import { IncomingMessage } from 'http';
+
+export default async function updateSpaceCreator(_: unknown, args: MutationUpdateSpaceCreatorArgs, context: IncomingMessage) {
+    const spaceById = await getSpaceById(args.spaceId);
+    checkEditSpacePermission(spaceById, context);
+    return prisma.space.update({
+        data: {
+            creator: args.creator
+        },
+        where: {
+            id: args.spaceId,
+        },
+    });
+}
