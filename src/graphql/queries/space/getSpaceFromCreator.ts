7b0ec1cc556f481da4faf5896c1660c158b1d9e3
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-20 19:42:34 +0500

    formatting changes

--- src/graphql/queries/space/getSpaceFromCreator.ts
+++ src/graphql/queries/space/getSpaceFromCreator.ts
@@ -2,11 +2,9 @@ import { QueryGetSpaceFromCreatorArgs } from '@/graphql/generated/graphql';
 import { prisma } from '@/prisma';
 
 export default function getSpaceFromCreator(_: any, { creatorId }: QueryGetSpaceFromCreatorArgs) {
-    return prisma.space.findFirstOrThrow(
-        {
-            where: {
-                creator: creatorId!,
-            }
-        }
-    );
+  return prisma.space.findFirstOrThrow({
+    where: {
+      creator: creatorId!,
+    },
+  });
 }
