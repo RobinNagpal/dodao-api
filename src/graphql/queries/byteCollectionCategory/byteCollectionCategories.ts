481927e8e2f601204b110d704e13df798b7435b6
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-11 20:18:35 +0500

    resolved lint error

--- src/graphql/queries/byteCollectionCategory/byteCollectionCategories.ts
+++ src/graphql/queries/byteCollectionCategory/byteCollectionCategories.ts
@@ -2,7 +2,7 @@ import { QueryByteCollectionCategoriesArgs } from '@/graphql/generated/graphql';
 import { prisma } from '@/prisma';
 
 export async function getByteCollectionCategories(spaceId: string) {
-  let byteCollectionCategories = await prisma.byteCollectionCategory.findMany({
+  const byteCollectionCategories = await prisma.byteCollectionCategory.findMany({
     where: {
       spaceId: spaceId,
     },
