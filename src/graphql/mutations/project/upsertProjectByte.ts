bba2985b0baf2aeae9d40863c1583e1d80941d3a
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-11 18:42:13 +0500

    some changes

--- src/graphql/mutations/project/upsertProjectByte.ts
+++ src/graphql/mutations/project/upsertProjectByte.ts
@@ -28,7 +28,7 @@ export default async function upsertProjectByte(_: unknown, args: MutationUpsert
         description: args.input.seoMeta?.description ?? args.input.content,
         keywords: args.input.seoMeta?.keywords ?? [],
       },
-      completionScreen: args.input.completionScreen ?? Prisma.JsonNull
+      completionScreen: args.input.completionScreen ?? Prisma.JsonNull,
     },
     update: {
       ...args.input,
@@ -41,7 +41,7 @@ export default async function upsertProjectByte(_: unknown, args: MutationUpsert
         description: args.input.seoMeta?.description ?? args.input.content,
         keywords: args.input.seoMeta?.keywords ?? [],
       },
-      completionScreen: args.input.completionScreen ?? Prisma.JsonNull
+      completionScreen: args.input.completionScreen ?? Prisma.JsonNull,
     },
     where: {
       id: args.input.id,
