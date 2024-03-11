f89a77ccec02b76597d46a52287af84082edaca1
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-11 20:35:51 +0500

    added new migration

---
+++ prisma/migrations/20240311150210_testing/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "bytes" ALTER COLUMN "completion_screen" SET DATA TYPE JSON;
+
+-- AlterTable
+ALTER TABLE "project_bytes" ALTER COLUMN "completion_screen" SET DATA TYPE JSON;
