1ba6cb70734f36bbd3d02e5ada5606a3e34a7598
Usman <u2003252@gmail.com>
2024-06-26 23:35:24 +0500

    Removed VideoAspectRatio from TidbitsCollection

--- src/graphql/mutations/byteCollection/updateByteCollection.ts
+++ src/graphql/mutations/byteCollection/updateByteCollection.ts
@@ -29,7 +29,6 @@ export default async function updateByteCollection(_: any, args: MutationUpdateB
       updatedAt: new Date(),
       priority: args.input.priority,
       videoUrl: args.input.videoUrl,
-      // videoAspectRatio: args.input.videoAspectRatio,
     },
   });
 
