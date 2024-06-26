a21f5bb862ff2b842b59cb8e4d0dfa4a29a1211c
Usman <u2003252@gmail.com>
2024-06-26 21:17:12 +0500

    Removed Aspect Ratio from Tidbit Collections

---
+++ prisma/migrations/20240626131415_without_video_aspect_ratio/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `video_aspect_ratio` on the `byte_collections` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "byte_collections" DROP COLUMN "video_aspect_ratio";
